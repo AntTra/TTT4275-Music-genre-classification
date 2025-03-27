ef0c95a2d8a5a5c7d84af2f3612d200995a1da7b
Vetlebrur <vetlerd@stud.ntnu.no>
2025-03-27 13:48:31 +0100

    Bruh moment...

--- task2.py
+++ task2.py
@@ -13,18 +13,10 @@ class kNN_Classifier:
         self.X_train = X
         self.Y_train = Y
 
-    def covariance(self):
-        covariance_matrix = np.cov(self.X_train, rowvar=False)
-        
-        return covariance_matrix
-    
     def euclidean_distance(self, X1, X2):
         d = sum((a - b)**2 for a, b in zip(X1, X2))
         return np.sqrt(d)
 
-    def mahalanobis_distance(self, X1, X2): #must make actual distance
-        d = sum((a - b)**2 for a, b in zip(X1, X2))
-        return np.sqrt(d)
     # Computational power increases with features and number of potential neighbors
     # Increasing k increases computational power?
     # Large k causes less distinction
@@ -59,13 +51,13 @@ df = pd.read_csv('Classification music/GenreClassData_30s.txt', delimiter='\t')
 df.columns = df.columns.str.strip()
 
 # TODO: Split code into train and test
-selected_features = ["spectral_rolloff_mean", "mfcc_1_mean", "spectral_centroid_mean", "tempo"]
+selected_features = ["spectral_rolloff_mean", "mfcc_1_mean", "spectral_centroid_mean", "tempo", "mfcc_2_mean", "mfcc_3_mean", "mfcc_4_mean", "mfcc_5_mean", "mfcc_6_mean", "mfcc_7_mean", "mfcc_8_mean", "mfcc_9_mean", "mfcc_10_mean", "mfcc_11_mean", "mfcc_12_mean","chroma_stft_1_mean", "chroma_stft_2_mean", "chroma_stft_3_mean", "chroma_stft_4_mean", "chroma_stft_5_mean", "chroma_stft_6_mean", "chroma_stft_7_mean", "chroma_stft_8_mean", "chroma_stft_9_mean", "chroma_stft_10_mean", "chroma_stft_11_mean", "chroma_stft_12_mean"]
 X = df[selected_features].values
 
 # Labels to genres
 y = df["GenreID"].values
-X_train, X_test = X[:800],X[800:]
-Y_train, Y_test = y[:800],y[800:]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 knn = kNN_Classifier(k=5)
 knn.fit(X_train, Y_train)
@@ -76,7 +68,7 @@ score = knn.score(X_test, Y_test)
 print("Predictions: ", prediction)
 print('Accuracy for ten genres: ', score*100, '%')
 
-selected_genres = ["pop", "disco", "metal", "classical"]#, "hiphop", "reggae", "blues", "rock", "jazz", "country"]
+selected_genres = ["pop", "disco", "metal", "classical"] #"hiphop", "reggae", "blues", "rock", "jazz", "country"]
 # Filter data by selected genres
 data_filtered = df[df["Genre"].isin(selected_genres)]
 
@@ -84,20 +76,19 @@ data_filtered = df[df["Genre"].isin(selected_genres)]
 summary_stats = data_filtered.groupby("Genre")[selected_features].describe()
 print("Summary Statistics by Genre:")
 print(summary_stats)
-print(knn.covariance())
-#Plot PDF of genres and features 
+
+# Plot PDF of genres and features 
 plt.figure(figsize=(14, 12))
 for i, feature in enumerate(selected_features):
-    ax = plt.subplot(2, 2, i+1)
+    plt.plot()
     for genre in selected_genres:
         subset = data_filtered[data_filtered["Genre"] == genre]
-        sns.kdeplot(subset[feature], label=genre, fill=True, common_norm=False, ax=ax)
-        ax.set_title("")
-    #plt.title(f"PDF of {feature}")
-    #plt.xlabel(feature)
+        sns.kdeplot(subset[feature], label=genre, fill=True, common_norm=False)
+    plt.title(f"PDF of {feature}")
+    plt.xlabel(feature)
     plt.ylabel("Density")
-#plt.tight_layout()
-plt.legend()
-plt.show()
+    plt.legend()
+    plt.show()
+
 
 
